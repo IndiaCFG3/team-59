b34253683d94a9b0a8491239aa992167bb09b60e
Nishchal J <nishchaljs@gmail.com>
2020-08-16 11:56:47 +0530

    merge

--- cfg/pannah/models.py
+++ cfg/pannah/models.py
@@ -4,23 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-    INCOME_GROUP = (
+    INCOME_GROUP =(
 		('belowAverage', 'belowAverage'),
 		('average','average'),
-		('aboveAverage', 'aboveAverage')
-	)
-	
-	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
-	income = models.FloatField(null=True)
-	income_category = models.CharField(max_length=200, null=True, choices=INCOME_GROUP)
-	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	gender = models.CharField(max_length=200, null=True, choices=GENDER_TYPE)
-	def __str__(self):
-		return self.name
+		('aboveAverage', 'aboveAverage'))
+    GENDER_TYPE = (
+        ('Male','Male'),
+        ('Female', 'Female'),
+        ('LGBTQ','LGBTQ')
+    )
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    income = models.FloatField(null=True)
+    income_category = models.CharField(max_length=200, null=True, choices=INCOME_GROUP)
+    profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    gender = models.CharField(max_length=200, null=True, choices=GENDER_TYPE)
+    def __str__(self):
+    	return self.name
 
 class volunteer(models.Model):
 	name = models.CharField(max_length=200, null=True)
@@ -40,7 +43,7 @@ class Scheme(models.Model):
     )
 	name = models.CharField(max_length=200, null=True)
 	income_category= models.CharField(max_length=200, null=True)
-	gender = models.CharField(max_length=200, choices=GENDER, null=True)
+	gender = models.CharField(max_length=200, choices=GENDER_TYPE, null=True)
 	state = models.CharField(max_length=200, null=True)
 	customer= models.ForeignKey(Customer, on_delete=models.CASCADE) 
 	#any other parameters
--- cfg/pannah/views.py
+++ cfg/pannah/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse("Home page")
+
+def schemes(request):
+	return HttpResponse("Scheme Page")
 
 # Create your views here.
