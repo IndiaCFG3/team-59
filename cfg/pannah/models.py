e8d16288bb5714053082ce485bbc93efdbc36298
Chinmay Desai <chinmaycm1@gmail.com>
2020-08-16 11:51:34 +0530

    config setup

--- cfg/pannah/models.py
+++ cfg/pannah/models.py
@@ -9,20 +9,24 @@ class Customer(models.Model):
 		('average','average'),
 		('aboveAverage', 'aboveAverage')
 	)
-	
-	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
-	income = models.FloatField(null=True)
-	income_category = models.CharField(max_length=200, null=True, choices=INCOME_GROUP)
-	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	gender = models.CharField(max_length=200, null=True, choices=GENDER_TYPE)
-	def __str__(self):
-		return self.name
+    GENDER_TYPE = (
+        ('Male','Male'),
+        ('Female', 'Female'),
+        ('LGBTQ','LGBTQ')
+    )
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    income = models.FloatField(null=True)
+    income_category = models.CharField(max_length=200, null=True, choices=INCOME_GROUP)
+    profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    gender = models.CharField(max_length=200, null=True, choices=GENDER_TYPE)
+    def __str__(self):
+        return self.name
 
-class volunteer(models.Model):
+class Volunteer(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
@@ -33,16 +37,16 @@ class volunteer(models.Model):
 		return self.name
 
 class Scheme(models.Model):
-	GENDER_TYPE = (
+    GENDER_TYPE = (
         ('Male','Male'),
         ('Female', 'Female'),
         ('LGBTQ','LGBTQ')
     )
-	name = models.CharField(max_length=200, null=True)
-	income_category= models.CharField(max_length=200, null=True)
-	gender = models.CharField(max_length=200, choices=GENDER, null=True)
-	state = models.CharField(max_length=200, null=True)
-	customer= models.ForeignKey(Customer, on_delete=models.CASCADE) 
+    name = models.CharField(max_length=200, null=True)
+    income_category= models.CharField(max_length=200, null=True)
+    gender = models.CharField(max_length=200, choices=GENDER_TYPE, null=True)
+    state = models.CharField(max_length=200, null=True)
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE) 
 	#any other parameters
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
